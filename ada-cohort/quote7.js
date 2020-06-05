485a510d36bec03956d3c9c608d8f3c33922283b
its-justus <ianjohnson9042@gmail.com>
2020-06-05 09:15:10 -0500

    added my name to export

--- ada-cohort/quote7.js
+++ ada-cohort/quote7.js
@@ -1,1 +1,1 @@
-module.exports = "In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move. - Douglas Adams 'The Hitchiker's Guide to the Galaxy'";
+module.exports = "Ian: 'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.' - Douglas Adams 'The Hitchiker's Guide to the Galaxy'";
